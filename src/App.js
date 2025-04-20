997dbc7bb65a4d0d2caee3027d26ed5ff01d1094
조영준 <shootinger@naver.com>
2025-04-20 15:34:33 +0900

    7장

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState, useRef } from 'react';
+import { useReducer, useRef } from 'react';
 
 import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
+import { type } from '@testing-library/user-event/dist/type';
 
 
 const MockTodo = [
@@ -12,30 +13,45 @@ const MockTodo = [
   { id: 2, content: "프로젝트 만들기", isDone: false, createdDate: new Date().getTime() },
 ];
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.newItem,...state];
+    case 'UPDATE':
+      return state.map((it) => 
+        it.id === action.targetId ? { ...it, isDone: !it.isDone } : it
+      );
+    case 'DELETE':
+      return state.filter((it) => it.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todo, setTodo] = useState(MockTodo); 
+  // const [todo, setTodo] = useState(MockTodo); 
+  const [todo,dispatch] = useReducer(reducer, MockTodo);
   const idRef = useRef(3);
+ 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    const nextTodo = [newTodo,...todo];
-    setTodo(nextTodo);
+    dispatch({
+      type: 'CREATE',
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
     idRef.current += 1;
   };
 
   const onUpdate = (targetId) => {
-    setTodo(todo.map((it) => 
-      it.id === targetId ? {...it, isDone: !it.isDone} : it
-    ));
-    
+    dispatch({ type: 'UPDATE', targetId });    
   };
 
   const onDelete = (targetId) => {
-    setTodo(todo.filter((it) => it.id !== targetId));
+    dispatch({ type: 'DELETE', targetId });    
   };
 
   return (<div className="App">
